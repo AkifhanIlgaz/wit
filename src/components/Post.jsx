82c1bda87221181f8fb27781217a0f77904ccd8b
AkifhanIlgaz <akifhanilgaz@gmail.com>
2023-08-28 11:32:04 +0300

    update Post component

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,11 +1,13 @@
 import { IonButton, IonButtons, IonCol, IonGrid, IonHeader, IonIcon, IonRow, IonTitle, IonToolbar } from '@ionic/react'
-import { bookmark, bookmarkOutline, chatbubbleOutline, chevronBackOutline, heart, heartOutline } from 'ionicons/icons'
+import { bookmark, bookmarkOutline, chatbubbleOutline, chevronBackOutline, heart, heartOutline, shareSocialOutline } from 'ionicons/icons'
 import { Fragment, useState } from 'react'
 
 const Post = ({ setIsOpen, post }) => {
 	const [isSaved, setIsSaved] = useState(false)
 	const [isLiked, setIsLiked] = useState(false)
 	const [likeCount, setLikeCount] = useState(post.likeCount)
+
+	// TODO: Update like count on firestore if it's different from initial count
 
 	return (
 		<Fragment>
@@ -16,15 +18,7 @@ const Post = ({ setIsOpen, post }) => {
 							<IonIcon icon={chevronBackOutline}></IonIcon>
 						</IonButton>
 					</IonButtons>
-					<IonTitle>
-						{/* TODO: Use app logo */}
-						Wear It Tomorrow
-					</IonTitle>
-					<IonButtons slot="end">
-						<IonButton onClick={() => setIsSaved(!isSaved)}>
-							<IonIcon icon={isSaved ? bookmark : bookmarkOutline}></IonIcon>
-						</IonButton>
-					</IonButtons>
+					<IonTitle>Wear It Tomorrow</IonTitle>
 				</IonToolbar>
 			</IonHeader>
 			<IonGrid className="ion-height ion-no-padding ion-no-margin">
@@ -35,22 +29,24 @@ const Post = ({ setIsOpen, post }) => {
 							<IonButtons slot="start">
 								<IonButton
 									onClick={() => {
-										if (isLiked === true) {
-											setLikeCount(l => l - 1)
-										} else {
-											setLikeCount(l => l + 1)
-										}
+										isLiked ? setLikeCount(l => l - 1) : setLikeCount(l => l + 1)
 										setIsLiked(!isLiked)
 									}}
 									color={'danger'}
 								>
-									<IonIcon icon={isLiked ? heart : heartOutline} slot="start"></IonIcon>
-									{likeCount}
+									<IonIcon icon={isLiked ? heart : heartOutline}></IonIcon>
 								</IonButton>
-							</IonButtons>
-							<IonButtons slot="end">
 								<IonButton>
 									<IonIcon icon={chatbubbleOutline}></IonIcon>
+								</IonButton>
+								<IonButton>
+									<IonIcon icon={shareSocialOutline}></IonIcon>
+								</IonButton>
+							</IonButtons>
+
+							<IonButtons slot="end">
+								<IonButton onClick={() => setIsSaved(!isSaved)}>
+									<IonIcon icon={isSaved ? bookmark : bookmarkOutline}></IonIcon>
 								</IonButton>
 							</IonButtons>
 						</IonToolbar>
