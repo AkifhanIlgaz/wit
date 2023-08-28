bfa828e0788788eeb11c7795145e5151bf6d9761
AkifhanIlgaz <akifhanilgaz@gmail.com>
2023-08-28 13:30:09 +0300

    render different profile page

--- src/components/ProfileAnalytics.jsx
+++ src/components/ProfileAnalytics.jsx
@@ -3,7 +3,7 @@ import AnalyticsBox from './AnalyticsBox'
 
 const ProfileAnalytics = ({ analytics }) => {
 	return (
-		<IonRow className="ion-align-items-center ion-justify-content-space-around ion-margin-top " style={{ borderTop: '1px solid rgb(219,219,219)', borderBottom: '1px solid rgb(219,219,219)' }}>
+		<IonRow className="ion-align-items-center ion-justify-content-space-around ion-margin-top  " style={{ borderTop: '1px solid rgb(219,219,219)', borderBottom: '1px solid rgb(219,219,219)' }}>
 			<IonCol
 				size="12"
 				className="ion-no-padding"
--- src/pages/MyProfile.jsx
+++ src/pages/MyProfile.jsx
@@ -1,9 +1,10 @@
-import { IonGrid } from '@ionic/react'
+import { IonButton, IonCol, IonGrid, IonIcon, IonRow } from '@ionic/react'
+import { settingsOutline } from 'ionicons/icons'
 import { useState } from 'react'
 import PostTabs from '../components/PostTabs'
 import Posts from '../components/Posts'
 import ProfileAnalytics from '../components/ProfileAnalytics'
-import ProfileHeader from '../components/ProfileHeader'
+import defaultProfilePhoto from '../images/defaultProfilePhoto.jpg'
 import Authorized from '../layouts/Authorized'
 
 const MyProfile = ({ userInfo }) => {
@@ -12,7 +13,40 @@ const MyProfile = ({ userInfo }) => {
 	return (
 		<Authorized>
 			<IonGrid className="ion-no-padding">
-				<ProfileHeader header={userInfo.header} />
+				<IonRow className="ion-align-items-center ion-justify-content-center ion-margin-top">
+					<IonCol
+						push=".4"
+						size="4"
+						style={{
+							paddingBottom: '0px',
+							paddingTop: '0px'
+						}}
+					>
+						<img src={userInfo.header.photoUrl || defaultProfilePhoto} alt="User Profile Photo" className="profile-photo" />
+					</IonCol>
+					<IonCol pull=".4" size="7">
+						<span className="username ion-no-margin">{userInfo.header.userName}</span>
+						<div
+							style={{
+								paddingTop: '5px'
+							}}
+						>
+							<IonButton
+								style={{
+									marginRight: '5px'
+								}}
+								className="button-text"
+								color={'button-background'}
+								size="small"
+							>
+								Edit Profile
+							</IonButton>
+							<IonButton color={'button-background'} size="small">
+								<IonIcon icon={settingsOutline}></IonIcon>
+							</IonButton>
+						</div>
+					</IonCol>
+				</IonRow>
 				<ProfileAnalytics analytics={userInfo.analytics} />
 			</IonGrid>
 			<PostTabs selectedTab={selectedTab} setSelectedTab={setSelectedTab} />
