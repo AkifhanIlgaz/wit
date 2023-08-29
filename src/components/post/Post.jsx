740a278118147b12a60150ff978b5b9803a279f4
AkifhanIlgaz <akifhanilgaz@gmail.com>
2023-08-29 17:07:32 +0300

    use useEffect teardown function to update liked & saved status

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,14 +1,20 @@
 import { IonButton, IonButtons, IonCol, IonGrid, IonIcon, IonRow, IonToolbar } from '@ionic/react'
 import { bookmark, bookmarkOutline, heart, heartOutline, shareSocialOutline } from 'ionicons/icons'
-import { Fragment, useState } from 'react'
+import { Fragment, useEffect, useState } from 'react'
 import ModalHeader from '../helper/ModalHeader'
 
 const Post = ({ setIsOpen, post }) => {
-	const [isSaved, setIsSaved] = useState(false)
-	const [isLiked, setIsLiked] = useState(false)
-	const [likeCount, setLikeCount] = useState(post.likeCount)
+	const [isLiked, setIsLiked] = useState(post.isLiked)
+	const [isSaved, setIsSaved] = useState(post.isSaved)
 
 	// TODO: Update like count on firestore if it's different from initial count
+
+	useEffect(() => {
+		return () => {
+			// TODO: Update liked & saved status on database
+			console.log('post liked: ', post.isLiked)
+		}
+	}, [])
 
 	return (
 		<Fragment>
@@ -21,8 +27,8 @@ const Post = ({ setIsOpen, post }) => {
 							<IonButtons slot="start">
 								<IonButton
 									onClick={() => {
-										isLiked ? setLikeCount(l => l - 1) : setLikeCount(l => l + 1)
 										setIsLiked(!isLiked)
+										post.isLiked = !post.isLiked
 									}}
 									color={'danger'}
 								>
@@ -34,7 +40,12 @@ const Post = ({ setIsOpen, post }) => {
 							</IonButtons>
 
 							<IonButtons slot="end">
-								<IonButton onClick={() => setIsSaved(!isSaved)}>
+								<IonButton
+									onClick={() => {
+										setIsSaved(!isSaved)
+										post.isSaved = !post.isSaved
+									}}
+								>
 									<IonIcon icon={isSaved ? bookmark : bookmarkOutline}></IonIcon>
 								</IonButton>
 							</IonButtons>
