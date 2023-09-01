4a7a91d4095cf598b9497747119be715d50b57e0
AkifhanIlgaz <akifhanilgaz@gmail.com>
2023-09-02 00:23:52 +0300

    show posts on card

--- src/components/post/Posts.jsx
+++ src/components/post/Posts.jsx
@@ -1,4 +1,4 @@
-import { IonCol, IonGrid, IonRow } from '@ionic/react'
+import { IonCard, IonCol, IonGrid, IonRow } from '@ionic/react'
 import { useHistory } from 'react-router'
 
 const splitIntoChunks = (arr, chunkSize) => {
@@ -10,7 +10,7 @@ const splitIntoChunks = (arr, chunkSize) => {
 }
 
 const Posts = ({ posts }) => {
-	const postChunks = splitIntoChunks(posts, 3)
+	const postChunks = splitIntoChunks(posts, 2)
 	const history = useHistory()
 	const pageWidth = document.body.clientWidth
 
@@ -19,45 +19,54 @@ const Posts = ({ posts }) => {
 			<IonGrid className="ion-no-padding post-grid">
 				{postChunks.map((chunk, i) => {
 					return (
-						<IonRow key={i} className="ion-justify-content-center">
-							<IonCol>
-								{chunk[0] && (
+						<IonRow key={i} className="ion-margin">
+							<IonCol
+								style={{
+									display: 'flex',
+									justifyContent: 'center',
+									alignItems: 'center'
+								}}
+								pull=".2"
+							>
+								<IonCard
+									className="ion-no-margin post-card"
+									style={{
+										width: '45vw',
+										height: '21vh'
+									}}
+								>
 									<img
 										src={chunk[0].photoUrl}
-										alt=""
-										height={pageWidth / 3}
-										width={pageWidth / 3}
-										onClick={() => {
-											history.push(`/posts/${chunk[0].postId}`)
-										}}
-									/>
-								)}
-							</IonCol>
-							<IonCol>
-								{chunk[1] && (
-									<img
-										src={chunk[1].photoUrl}
-										alt=""
-										height={pageWidth / 3}
-										width={pageWidth / 3}
+										height={'100%'}
 										onClick={() => {
 											history.push(`/posts/${chunk[1].postId}`)
 										}}
 									/>
-								)}
+								</IonCard>
 							</IonCol>
-							<IonCol>
-								{chunk[2] && (
+							<IonCol
+								style={{
+									display: 'flex',
+									justifyContent: 'center',
+									alignItems: 'center'
+								}}
+								push=".2"
+							>
+								<IonCard
+									className="ion-no-margin post-card"
+									style={{
+										width: '45vw',
+										height: '21vh'
+									}}
+								>
 									<img
-										src={chunk[2].photoUrl}
-										alt=""
-										height={pageWidth / 3}
-										width={pageWidth / 3}
+										src={chunk[1].photoUrl}
+										height={'100%'}
 										onClick={() => {
-											history.push(`/posts/${chunk[2].postId}`)
+											history.push(`/posts/${chunk[1].postId}`)
 										}}
 									/>
-								)}
+								</IonCard>
 							</IonCol>
 						</IonRow>
 					)
