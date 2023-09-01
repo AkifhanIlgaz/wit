8fbe767a895ae7f356d0c112ca062323d898c7b0
AkifhanIlgaz <akifhanilgaz@gmail.com>
2023-09-01 21:49:27 +0300

    open an action sheet when user tries to unfollow

--- src/components/buttons/FollowButton.jsx
+++ src/components/buttons/FollowButton.jsx
@@ -1,9 +1,10 @@
-import { IonButton, IonIcon } from '@ionic/react'
+import { IonActionSheet, IonButton, IonIcon } from '@ionic/react'
 import { checkmark } from 'ionicons/icons'
-import { useState } from 'react'
+import { Fragment, useState } from 'react'
 
 const FollowButton = ({ isFollowed }) => {
 	const [stateIsFollowed, setStateIsFollowed] = useState(isFollowed)
+	const [isActionSheetOpen, setIsActionSheetOpen] = useState(false)
 
 	return stateIsFollowed ? (
 		<IonButton
@@ -18,18 +19,37 @@ const FollowButton = ({ isFollowed }) => {
 			Follow
 		</IonButton>
 	) : (
-		<IonButton
-			size="small"
-			color={'tertiary'}
-			className="button-text "
-			style={{
-				marginRight: '5px'
-			}}
-			onClick={() => setStateIsFollowed(!stateIsFollowed)}
-		>
-			<IonIcon icon={checkmark} slot="end"></IonIcon>
-			Following
-		</IonButton>
+		<Fragment>
+			<IonButton
+				size="small"
+				color={'tertiary'}
+				className="button-text "
+				style={{
+					marginRight: '5px'
+				}}
+				onClick={() => setIsActionSheetOpen(true)}
+			>
+				<IonIcon icon={checkmark} slot="end"></IonIcon>
+				Following
+			</IonButton>
+			<IonActionSheet
+				isOpen={isActionSheetOpen}
+				buttons={[
+					{
+						text: 'Unfollow',
+						role: 'destructive',
+						handler: () => {
+							setStateIsFollowed(!stateIsFollowed)
+						}
+					},
+					{
+						text: 'Cancel',
+						role: 'cancel'
+					}
+				]}
+				onDidDismiss={() => setIsActionSheetOpen(false)}
+			></IonActionSheet>
+		</Fragment>
 	)
 }
 
