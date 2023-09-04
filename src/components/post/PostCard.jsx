d63c6c82f6988d6b1dcfcff8633c5d6fea95f2ca
AkifhanIlgaz <akifhanilgaz@gmail.com>
2023-09-04 10:01:12 +0300

    add infinite scroll to home page

--- src/components/post/PostCard.jsx
+++ src/components/post/PostCard.jsx
@@ -3,21 +3,8 @@ import { useHistory } from 'react-router'
 import PostHeader from './PostHeader'
 import PostToolbar from './PostToolbar'
 
-const PostCard = () => {
+const PostCard = ({ postInfo }) => {
 	const history = useHistory()
-
-	const postInfo = {
-		profilePhoto: 'http://dummyimage.com/277x226.png/ff4444/ffffff',
-		displayName: 'Gaye Su Akyol',
-		username: 'gayesuakyol',
-		isFollowed: false,
-		postId: '64edcf7dfc13ae35b0ad21eb',
-		likeCount: 10564,
-		isLiked: true,
-		isSaved: true,
-		createdAt: '7/18/2023',
-		photoUrl: 'http://dummyimage.com/208x100.png/dddddd/000000'
-	}
 
 	return (
 		<IonGrid className="ion-no-padding ">
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,54 @@
+import { IonInfiniteScroll, IonInfiniteScrollContent, IonList } from '@ionic/react'
+import { useEffect, useState } from 'react'
 import PostCard from '../components/post/PostCard'
 import Authorized from '../layouts/Authorized'
 
 const Home = () => {
+	const [posts, setPosts] = useState([])
+	const [postChunkIndex, setPostChunkIndex] = useState(0)
+
+	const getPosts = postChunkIndex => {
+		const postInfo = {
+			profilePhoto: 'http://dummyimage.com/277x226.png/ff4444/ffffff',
+			displayName: `Gaye Su Akyol | Index ${postChunkIndex}`,
+			username: 'gayesuakyol',
+			isFollowed: false,
+			postId: '64edcf7dfc13ae35b0ad21eb',
+			likeCount: 10564,
+			isLiked: true,
+			isSaved: true,
+			createdAt: '7/18/2023',
+			photoUrl: 'http://dummyimage.com/208x100.png/dddddd/000000'
+		}
+
+		setTimeout(() => {
+			setPosts([...posts, ...new Array(3).fill({ ...postInfo })])
+		}, 1000)
+	}
+
+	useEffect(() => {
+		getPosts(postChunkIndex)
+	}, [])
+
 	return (
 		<Authorized>
-			<PostCard />
-			<PostCard />
-			<PostCard />
+			<IonList>
+				{posts.map((post, index) => {
+					return <PostCard key={index} postInfo={post} />
+				})}
+			</IonList>
+
+			<IonInfiniteScroll
+				onIonInfinite={ev => {
+					setPostChunkIndex(s => s + 1)
+					getPosts(postChunkIndex + 1)
+					setTimeout(() => {
+						ev.target.complete()
+					}, 1000)
+				}}
+			>
+				<IonInfiniteScrollContent></IonInfiniteScrollContent>
+			</IonInfiniteScroll>
 		</Authorized>
 	)
 }
