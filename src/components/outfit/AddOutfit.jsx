084340f9b01dc336761a1ef81ddedbe86a495c68
AkifhanIlgaz <akifhanilgaz@gmail.com>
2023-09-05 12:12:30 +0300

    take link info with alert

--- src/components/outfit/AddOutfit.jsx
+++ src/components/outfit/AddOutfit.jsx
@@ -1,8 +1,9 @@
-import { IonButton, IonButtons, IonCard, IonCardContent, IonModal, IonPopover, IonToolbar } from '@ionic/react'
+import { IonAlert, IonButton, IonButtons, IonCard, IonCardContent, IonModal, IonPopover, IonToolbar } from '@ionic/react'
 import { useState } from 'react'
 
 const AddOutfit = ({ isOpen, setIsOpen, photo }) => {
 	const [point, setPoint] = useState({})
+	const [isAlertOpen, setIsAlertOpen] = useState(false)
 
 	return (
 		<IonModal isOpen={isOpen}>
@@ -41,26 +42,48 @@ const AddOutfit = ({ isOpen, setIsOpen, photo }) => {
 							y: e.clientY - rect.y
 						}
 
-						console.log(clicked)
-
 						const button = {
 							left: (clicked.x / rect.width) * 100,
 							top: (clicked.y / rect.height) * 100
 						}
 
+						setIsAlertOpen(true)
 						setPoint(button)
 					}}
 				/>
 
-				<span
-					style={{
-						position: 'absolute',
-						left: `${point.left}%`,
-						top: `${point.top}%`
-					}}
-				>
-					Clicked
-				</span>
+				<IonAlert
+					isOpen={isAlertOpen}
+					header="Please enter your info"
+					buttons={[
+						{
+							text: 'Cancel',
+							role: 'destructive',
+							handler: () => {
+								// TODO: Reset point ?
+							}
+						},
+						{
+							text: 'Save',
+							role: 'confirm',
+							handler: () => {
+								// TODO: Reset point ?
+							}
+						}
+					]}
+					inputs={[
+						{
+							type: 'text',
+							placeholder: 'Title'
+						},
+
+						{
+							type: 'url',
+							placeholder: 'Link'
+						}
+					]}
+					onDidDismiss={() => setIsAlertOpen(false)}
+				></IonAlert>
 			</div>
 		</IonModal>
 	)
