fbec83814dd67dd77843924734e3c9230a76b8cd
AkifhanIlgaz <akifhanilgaz@gmail.com>
2023-09-05 15:42:35 +0300

    alert if the given url isn't valid

---
+++ src/components/outfit/AddLinks.jsx
@@ -0,0 +1,98 @@
+import { IonAlert } from '@ionic/react'
+import isUrlHttp from 'is-url-http'
+import { useState } from 'react'
+import OutfitLinkDot from './OutfitLinkDot'
+
+const AddLinks = ({ photo }) => {
+	const [currentDot, setCurrentDot] = useState({})
+	const [isAlertOpen, setIsAlertOpen] = useState(false)
+	const [isErrorOpen, setIsErrorOpen] = useState(false)
+	const errorMessage = 'Please enter an valid URL'
+	const [links, setLinks] = useState([])
+
+	return (
+		<div
+			style={{
+				position: 'relative'
+			}}
+		>
+			<img
+				src={photo}
+				style={{
+					width: '100vw',
+					height: '100vh'
+				}}
+				onClick={e => {
+					const rect = e.currentTarget.getBoundingClientRect()
+
+					// Relative to clicked image
+					const clicked = {
+						x: e.clientX - rect.x,
+						y: e.clientY - rect.y
+					}
+
+					const point = {
+						left: (clicked.x / rect.width) * 100,
+						top: (clicked.y / rect.height) * 100
+					}
+
+					setIsAlertOpen(true)
+					setCurrentDot(point)
+				}}
+			/>
+			<IonAlert
+				isOpen={isAlertOpen}
+				header="Please enter your info"
+				buttons={[
+					{
+						text: 'Cancel',
+						role: 'destructive',
+						handler: () => {
+							setCurrentDot({})
+							setLinks([])
+						}
+					},
+					{
+						text: 'Save',
+						role: 'confirm',
+						handler: data => {
+							// TODO: If it's not valid url alert
+
+							if (isUrlHttp(data['0'])) {
+								setLinks([
+									...links,
+									{
+										left: `${currentDot.left - 2}%`,
+										top: `${currentDot.top - 1}%`,
+										url: data['0']
+									}
+								])
+							} else {
+								setIsErrorOpen(true)
+							}
+
+							// TODO: Reset point ?
+						}
+					}
+				]}
+				inputs={[
+					{
+						type: 'url',
+						placeholder: 'Link'
+					}
+				]}
+				onDidDismiss={() => {
+					setIsAlertOpen(false)
+				}}
+			></IonAlert>
+
+			{links.map(link => (
+				<OutfitLinkDot link={link} />
+			))}
+
+			<IonAlert isOpen={isErrorOpen} message={errorMessage} buttons={['OK']} onDidDismiss={() => setIsErrorOpen(false)}></IonAlert>
+		</div>
+	)
+}
+
+export default AddLinks
--- src/components/outfit/AddOutfit.jsx
+++ src/components/outfit/AddOutfit.jsx
@@ -1,7 +1,6 @@
-import { IonAlert, IonButton, IonButtons, IonCard, IonCardContent, IonModal, IonPopover, IonToolbar } from '@ionic/react'
-import isUrlHttp from 'is-url-http'
+import { IonButton, IonButtons, IonCard, IonCardContent, IonModal, IonPopover, IonToolbar } from '@ionic/react'
 import { useState } from 'react'
-import OutfitLinkDot from './OutfitLinkDot'
+import AddLinks from './AddLinks'
 
 const AddOutfit = ({ isOpen, setIsOpen, photo }) => {
 	const [dot, setDot] = useState({})
@@ -32,83 +31,8 @@ const AddOutfit = ({ isOpen, setIsOpen, photo }) => {
 					<IonButton onClick={close}>Save</IonButton>
 				</IonButtons>
 			</IonToolbar>
-			<div
-				style={{
-					position: 'relative'
-				}}
-			>
-				<img
-					src={photo}
-					style={{
-						width: '100vw',
-						height: '100vh'
-					}}
-					onClick={e => {
-						const rect = e.currentTarget.getBoundingClientRect()
 
-						// Relative to clicked image
-						const clicked = {
-							x: e.clientX - rect.x,
-							y: e.clientY - rect.y
-						}
-
-						const point = {
-							left: (clicked.x / rect.width) * 100,
-							top: (clicked.y / rect.height) * 100
-						}
-
-						setIsAlertOpen(true)
-						setDot(point)
-					}}
-				/>
-
-				<IonAlert
-					isOpen={isAlertOpen}
-					header="Please enter your info"
-					buttons={[
-						{
-							text: 'Cancel',
-							role: 'destructive',
-							handler: () => {
-								setDot({})
-								setLinks([])
-							}
-						},
-						{
-							text: 'Save',
-							role: 'confirm',
-							handler: data => {
-								// TODO: If it's not valid url alert
-								if (isUrlHttp(data['0'])) {
-									setLinks([
-										...links,
-										{
-											left: `${dot.left - 2}%`,
-											top: `${dot.top - 1}%`,
-											url: data['0']
-										}
-									])
-								}
-
-								// TODO: Reset point ?
-							}
-						}
-					]}
-					inputs={[
-						{
-							type: 'url',
-							placeholder: 'Link'
-						}
-					]}
-					onDidDismiss={e => {
-						setIsAlertOpen(false)
-					}}
-				></IonAlert>
-
-				{links.map(link => (
-					<OutfitLinkDot link={link} />
-				))}
-			</div>
+			<AddLinks photo={photo} />
 		</IonModal>
 	)
 }
