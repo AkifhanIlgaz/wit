8852fb1e0b17fc17d6175dde0cd1c45e9ecafcb7
AkifhanIlgaz <akifhanilgaz@gmail.com>
2023-09-08 10:06:09 +0300

    use controller alert

--- src/components/outfit/AddLinks.jsx
+++ src/components/outfit/AddLinks.jsx
@@ -1,4 +1,4 @@
-import { IonAlert } from '@ionic/react'
+import { IonAlert, useIonAlert } from '@ionic/react'
 import isUrlHttp from 'is-url-http'
 import { useState } from 'react'
 import OutfitLinkDot from './OutfitLinkDot'
@@ -12,13 +12,12 @@ const AddLinks = ({ photo }) => {
 	const invalidTitle = 'Please enter title'
 	const [links, setLinks] = useState([])
 
-	const [titleValue, setTitleValue] = useState('')
-	const [linkValue, setLinkValue] = useState('')
-
 	const reset = () => {
 		setCurrentDot({})
 		setLinks([])
 	}
+
+	const [presentAlert] = useIonAlert()
 
 	return (
 		<div
@@ -47,75 +46,63 @@ const AddLinks = ({ photo }) => {
 					}
 
 					setIsAlertOpen(true)
+					presentAlert({
+						header: 'Please enter your info',
+						buttons: [
+							{
+								text: 'Cancel',
+								role: 'destructive',
+								handler: () => {
+									setCurrentDot({})
+								}
+							},
+							{
+								text: 'Save',
+								role: 'confirm',
+								handler: data => {
+									console.log(data)
+									if (data.title === '') {
+										setIsErrorOpen(true)
+										setErrorMessage(invalidTitle)
+
+										setIsAlertOpen(false)
+										return
+									}
+									if (!isUrlHttp(data.link)) {
+										setIsErrorOpen(true)
+										setErrorMessage(invalidUrl)
+										return
+									}
+
+									setLinks([
+										...links,
+										{
+											left: `${currentDot.left - 2}%`,
+											top: `${currentDot.top - 1}%`,
+											title: data.title,
+											url: data.link
+										}
+									])
+								}
+							}
+						],
+						inputs: [
+							{
+								name: 'text',
+								type: 'text',
+								placeholder: 'Title'
+							},
+							{
+								name: 'link',
+								type: 'url',
+								placeholder: 'Link'
+							}
+						]
+					})
+
 					setCurrentDot(point)
 				}}
 			/>
-			<IonAlert
-				isOpen={isAlertOpen}
-				header="Please enter your info"
-				buttons={[
-					{
-						text: 'Cancel',
-						role: 'destructive',
-						handler: () => {
-							setCurrentDot({})
-						}
-					},
-					{
-						text: 'Save',
-						role: 'confirm',
-						handler: data => {
-							if (data.title === '') {
-								setIsErrorOpen(true)
-								setErrorMessage(invalidTitle)
-
-								setIsAlertOpen(false)
-								return
-							}
-							if (!isUrlHttp(data.link)) {
-								setIsErrorOpen(true)
-								setErrorMessage(invalidUrl)
-								return
-							}
-
-							setLinks([
-								...links,
-								{
-									left: `${currentDot.left - 2}%`,
-									top: `${currentDot.top - 1}%`,
-									title: data.title,
-									url: data.link
-								}
-							])
-						}
-					}
-				]}
-				inputs={[
-					{
-						name: 'text',
-						type: 'text',
-						placeholder: 'Title',
-						value: titleValue
-					},
-					{
-						name: 'link',
-						type: 'url',
-						placeholder: 'Link',
-						value: linkValue
-					}
-				]}
-				onDidDismiss={() => {
-					setTitleValue('')
-					setLinkValue('')
-					setIsAlertOpen(false)
-				}}
-				onIonAlertWillDismiss={e => {
-					console.log(e)
-					setTitleValue('')
-					setLinkValue('')
-					setIsAlertOpen(false)
-				}}
-			></IonAlert>
 
 			{links.map((link, index) => (
 				<OutfitLinkDot key={index} link={link} />
