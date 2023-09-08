be48d04b24c8a94fa7ce762617fddac1719d582e
AkifhanIlgaz <akifhanilgaz@gmail.com>
2023-09-08 10:17:07 +0300

    move toolbar to AddLinks component

--- src/components/outfit/AddLinks.jsx
+++ src/components/outfit/AddLinks.jsx
@@ -1,11 +1,10 @@
-import { IonAlert, useIonAlert } from '@ionic/react'
+import { IonAlert, IonButton, IonButtons, IonToolbar, useIonAlert } from '@ionic/react'
 import isUrlHttp from 'is-url-http'
-import { useState } from 'react'
+import { Fragment, useState } from 'react'
 import OutfitLinkDot from './OutfitLinkDot'
 
-const AddLinks = ({ photo }) => {
-	const [currentDot, setCurrentDot] = useState({})
-	const [isAlertOpen, setIsAlertOpen] = useState(false)
+const AddLinks = ({ photo, setIsOpen }) => {
+	const [currentPoint, setCurrentPoint] = useState({})
 	const [isErrorOpen, setIsErrorOpen] = useState(false)
 	const [errorMessage, setErrorMessage] = useState('')
 	const invalidUrl = 'Please enter an valid URL'
@@ -13,103 +12,113 @@ const AddLinks = ({ photo }) => {
 	const [links, setLinks] = useState([])
 
 	const reset = () => {
-		setCurrentDot({})
+		setCurrentPoint({})
 		setLinks([])
+		setIsOpen(false)
 	}
 
 	const [presentAlert] = useIonAlert()
 
 	return (
-		<div
-			style={{
-				position: 'relative'
-			}}
-		>
-			<img
-				src={photo}
+		<Fragment>
+			<IonToolbar color={'transparent'}>
+				<IonButtons slot="start">
+					<IonButton onClick={reset}>Cancel</IonButton>
+				</IonButtons>
+				<IonButtons slot="end">
+					<IonButton onClick={reset}>Save</IonButton>
+				</IonButtons>
+			</IonToolbar>
+			<div
 				style={{
-					width: '100vw',
-					height: '100vh'
+					position: 'relative'
 				}}
-				onClick={e => {
-					const rect = e.currentTarget.getBoundingClientRect()
+			>
+				<img
+					src={photo}
+					style={{
+						width: '100vw',
+						height: '100vh'
+					}}
+					onClick={e => {
+						const rect = e.currentTarget.getBoundingClientRect()
 
-					// Relative to clicked image
-					const clicked = {
-						x: e.clientX - rect.x,
-						y: e.clientY - rect.y
-					}
+						// Relative to clicked image
+						const clicked = {
+							x: e.clientX - rect.x,
+							y: e.clientY - rect.y
+						}
 
-					const point = {
-						left: (clicked.x / rect.width) * 100,
-						top: (clicked.y / rect.height) * 100
-					}
+						const point = {
+							left: (clicked.x / rect.width) * 100,
+							top: (clicked.y / rect.height) * 100
+						}
 
-					setIsAlertOpen(true)
-					presentAlert({
-						header: 'Please enter your info',
-						buttons: [
-							{
-								text: 'Cancel',
-								role: 'destructive',
-								handler: () => {
-									setCurrentDot({})
-								}
-							},
-							{
-								text: 'Save',
-								role: 'confirm',
-								handler: data => {
-									console.log(data)
-									if (data.title === '') {
-										setIsErrorOpen(true)
-										setErrorMessage(invalidTitle)
+						setCurrentPoint(point)
 
-										setIsAlertOpen(false)
-										return
+						presentAlert({
+							header: 'Please enter your info',
+							buttons: [
+								{
+									text: 'Cancel',
+									role: 'destructive',
+									handler: () => {
+										setCurrentPoint({})
 									}
-									if (!isUrlHttp(data.link)) {
-										setIsErrorOpen(true)
-										setErrorMessage(invalidUrl)
-										return
-									}
+								},
+								{
+									text: 'Save',
+									role: 'confirm',
+									handler: data => {
+										console.log(data)
+										if (data.title === '') {
+											setIsErrorOpen(true)
+											setErrorMessage(invalidTitle)
 
-									setLinks([
-										...links,
-										{
-											left: `${currentDot.left - 2}%`,
-											top: `${currentDot.top - 1}%`,
-											title: data.title,
-											url: data.link
+											setIsAlertOpen(false)
+											return
 										}
-									])
+										if (!isUrlHttp(data.link)) {
+											setIsErrorOpen(true)
+											setErrorMessage(invalidUrl)
+											return
+										}
+
+										setLinks([
+											...links,
+											{
+												left: `${currentPoint.left - 2}%`,
+												top: `${currentPoint.top - 1}%`,
+												title: data.title,
+												url: data.link
+											}
+										])
+									}
 								}
-							}
-						],
-						inputs: [
-							{
-								name: 'text',
-								type: 'text',
-								placeholder: 'Title'
-							},
-							{
-								name: 'link',
-								type: 'url',
-								placeholder: 'Link'
-							}
-						]
-					})
+							],
+							inputs: [
+								{
+									name: 'text',
+									type: 'text',
+									placeholder: 'Title'
+								},
+								{
+									name: 'link',
+									type: 'url',
+									placeholder: 'Link'
+								}
+							]
+						})
+					}}
+				/>
 
-					setCurrentDot(point)
-				}}
-			/>
+				{links.map((link, index) => (
+					<OutfitLinkDot key={index} link={link} />
+				))}
 
-			{links.map((link, index) => (
-				<OutfitLinkDot key={index} link={link} />
-			))}
-
-			<IonAlert isOpen={isErrorOpen} message={errorMessage} buttons={['OK']} onDidDismiss={() => setIsErrorOpen(false)}></IonAlert>
-		</div>
+				<IonAlert isOpen={isErrorOpen} message={errorMessage} buttons={['OK']} onDidDismiss={() => setIsErrorOpen(false)}></IonAlert>
+			</div>
+		</Fragment>
 	)
 }
 
--- src/components/outfit/AddOutfit.jsx
+++ src/components/outfit/AddOutfit.jsx
@@ -1,20 +1,7 @@
-import { IonButton, IonButtons, IonCard, IonCardContent, IonModal, IonPopover, IonToolbar } from '@ionic/react'
-import { useState } from 'react'
+import { IonCard, IonCardContent, IonModal, IonPopover } from '@ionic/react'
 import AddLinks from './AddLinks'
 
 const AddOutfit = ({ isOpen, setIsOpen, photo }) => {
-	const [dot, setDot] = useState({})
-	const [isAlertOpen, setIsAlertOpen] = useState(false)
-
-	//    Left, right, title, url
-	const [links, setLinks] = useState([])
-
-	const close = () => {
-		setDot({})
-		setLinks([])
-		setIsOpen(false)
-	}
-
 	return (
 		<IonModal isOpen={isOpen}>
 			<IonPopover isOpen dismissOnSelect>
@@ -23,16 +10,7 @@ const AddOutfit = ({ isOpen, setIsOpen, photo }) => {
 				</IonCard>
 			</IonPopover>
 
-			<IonToolbar color={'transparent'}>
-				<IonButtons slot="start">
-					<IonButton onClick={close}>Cancel</IonButton>
-				</IonButtons>
-				<IonButtons slot="end">
-					<IonButton onClick={close}>Save</IonButton>
-				</IonButtons>
-			</IonToolbar>
-
-			<AddLinks photo={photo} />
+			<AddLinks photo={photo} setIsOpen={setIsOpen} />
 		</IonModal>
 	)
 }
