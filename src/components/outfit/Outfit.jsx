1eaf6211358473798714e2130bb644b75062f431
AkifhanIlgaz <akifhanilgaz@gmail.com>
2023-10-06 10:49:15 +0300

    remove link from outfit

--- src/components/outfit/Outfit.jsx
+++ src/components/outfit/Outfit.jsx
@@ -1,15 +1,62 @@
 import { IonButton, IonButtons, IonContent, IonHeader, IonIcon, IonPage, IonTitle, IonToolbar } from '@ionic/react'
 import { chevronBackOutline } from 'ionicons/icons'
+import { useEffect } from 'react'
+import { useAuthState } from 'react-firebase-hooks/auth'
 import { useHistory } from 'react-router'
-import { useRecoilState } from 'recoil'
+import { useRecoilState, useRecoilValue } from 'recoil'
+import Firebase from '../../api/firebase/firebase'
+import { baseUrl, updateLink } from '../../api/wit-api/endPoints'
 import outfitState from '../../atoms/outfit'
+import userState from '../../atoms/user'
 import defaultPostPhoto from '../../images/defaultPostPhoto.jpg'
+import EditableOutfitLinkDot from './EditableOutfitLinkDot'
 import OutfitLinkDot from './OutfitLinkDot'
 
 // TODO: Add links props
 const Outfit = () => {
 	const history = useHistory()
 	const [outfit, setOutfit] = useRecoilState(outfitState)
+	const user = useRecoilValue(userState)
+	const firebase = new Firebase()
+	const [currentUser, loading] = useAuthState(firebase.auth)
+	// Remove link && Update link
+
+	/*
+	TODO: Make an API call to backend if success	
+	TODO: Then, update currentOutfit recoil state
+	*/
+
+	const removeOutfitLink = async index => {
+		const idToken = await currentUser.getIdToken(true)
+
+		const link = outfit.links[index]
+
+		console.log(link)
+		const res = await fetch(`${baseUrl}${updateLink}?` + new URLSearchParams({ outfitId: outfit.id }), {
+			method: 'DELETE',
+			headers: {
+				Authorization: idToken,
+				ContentType: 'application/json'
+			},
+			body: JSON.stringify(link)
+		})
+
+		if (res.ok) {
+			const copyLinks = [...outfit.links]
+			copyLinks.splice(index, 1)
+			setOutfit({ ...outfit, links: copyLinks })
+		}
+
+		console.log(res)
+	}
+
+	const updateOutfitLink = async () => {
+		const idToken = await currentUser.getIdToken(true)
+	}
+
+	useEffect(() => {
+		if (loading) return
+	}, [])
 
 	return (
 		<IonPage>
@@ -37,7 +84,7 @@ const Outfit = () => {
 						}}
 					/>
 
-					{outfit.links && outfit.links.map((link, index) => <OutfitLinkDot key={index} link={link} />)}
+					{outfit.uid === user.uid ? outfit.links && outfit.links.map((link, index) => <EditableOutfitLinkDot key={index} link={link} removeLink={() => removeOutfitLink(index)} updateLink={updateOutfitLink} />) : outfit.links && outfit.links.map((link, index) => <OutfitLinkDot key={index} link={link} />)}
 				</div>
 			</IonContent>
 		</IonPage>
